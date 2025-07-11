bfd5a1a5b5dd2526d6a29eb8e36add5bcacfe5a9
kachhaplily <lilykumarikachhap@gmail.com>
2025-07-11 14:12:03 +0530

    Add core java

---
+++ Hello.java
@@ -0,0 +1,19 @@
+
+//data Type
+public class Hello{
+    public static void main(String args[]){
+     int age = 25;
+        double price = 99.99;
+        char grade = 'A';
+        boolean isPassed = true;
+        String name = "Lily";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Price: " + price);
+        System.out.println("Grade: " + grade);
+        System.out.println("Passed: " + isPassed);
+
+        //Literals
+    }
+}
---
+++ TypeCaste.java
@@ -0,0 +1,17 @@
+class TypeCaste{
+    public static void main(String args[]){
+        // byte b = 127;
+        // int a= b;
+        int num1= 493204832;
+        
+        byte k = (byte)num1;
+
+        byte num2=8;
+        byte num3=89;
+        int sum = num2*num3;
+        System.out.println(sum);
+
+
+        System.out.println(k);
+    }
+}
