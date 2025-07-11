cba20c81d106930f564eba109036617a1075f0ff
kachhaplily <lilykumarikachhap@gmail.com>
2025-07-11 14:49:19 +0530

    RelationOpration

---
+++ RelationOp.java
@@ -0,0 +1,13 @@
+class RelationOp{
+    public static void main(String args[]){
+
+        int num1 = 5;
+        byte num2=8;
+        System.out.println(num1!=num2);
+        System.out.println(num1==5);
+                System.out.println(num1>num2);
+                System.out.println(num1<num2);
+                System.out.println(num1>=num2);
+
+    }
+}
